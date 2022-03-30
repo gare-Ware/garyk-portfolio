ac89030407920632dd49dc425ebf11f198a70374
gareWare <92345400+gare-Ware@users.noreply.github.com>
2022-03-30 17:56:45 -0400

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,6 +7,7 @@ export default function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false)
     
     const toggleMenu = () => setMenuOpen(!menuOpen)
+    const closeMenu = () => setMenuOpen(false)
 
     return (
         <>
@@ -14,11 +15,11 @@ export default function Navbar() {
                 <div className={menuOpen ? "navbar__burger open" : "navbar__burger"} onClick={toggleMenu} >
                     <div className="navbar__burger-btn"></div>
                 </div>
-                <HashLink smooth to="/#contact" className="navbar__contact" onClick={toggleMenu}>Contact</HashLink>
+                <HashLink smooth to="/#contact" className="navbar__contact" onClick={closeMenu}>Contact</HashLink>
             </header>
             <nav className={menuOpen ? "navbar__menu active" : "navbar__menu"}>
                 <h1 className="navbar__menu-header">Jump to</h1>
-                <ul className="navbar__menu-items" onClick={toggleMenu}>
+                <ul className="navbar__menu-items" onClick={closeMenu}>
                     <li><HashLink smooth to="/#top">Home</HashLink></li>
                     <li><HashLink smooth to="/#about">About</HashLink></li>
                     <li><HashLink smooth to="/#recent-projects">Recent projects</HashLink></li>
