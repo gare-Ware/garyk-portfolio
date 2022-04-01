a7d39429e50a7851c5f96e088d9aaf52dfb985d0
gareWare <92345400+gare-Ware@users.noreply.github.com>
2022-03-31 22:38:29 -0400

    Beta build

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,27 +1,63 @@
+import { useState } from 'react'
 import { FaRegEnvelope, FaLinkedinIn, FaTwitter } from 'react-icons/fa'
 
 export default function Contact() {
+    
+    const [subjectInput, setSubjectInput] = useState("")
+    const [messageInput, setMessageInput] = useState("")
+    
+    const handleChange = e => {
+        const value = e.target.value
+        const name = e.target.name
+        name === 'subjectInput'
+            ? setSubjectInput(value)
+            : setMessageInput(value)
+    }
+
     return (
         <div id='contact' className="container contact">
             <h1 className="contact__header">Contact me</h1>
+            <div className='contact__handles'>
+                <a href="home" className="contact__link">
+                    <p className="contact__link-type">Email <FaRegEnvelope className='contact__link-type-icon'/></p>
+                </a>
+                <p className="contact__link-handle">gary@garyk.dev</p>
+                <a href="https://www.linkedin.com/in/garykassee/" target="_blank" rel="noreferrer" className="contact__link">
+                    <p className="contact__link-type">LinkedIn <FaLinkedinIn className='contact__link-type-icon'/></p>
+                </a>
+                <p className="contact__link-handle">/in/garykassee</p>
+                <a href="https://twitter.com/gareWare" target="_blank" rel="noreferrer" className="contact__link">
+                    <p className="contact__link-type">Twitter <FaTwitter className='contact__link-type-icon'/></p>
+                </a>
+                <p className="contact__link-handle">gareWare</p>
+            </div>
+            <div className='contact__message'>
+                <h2 className="contact__message-header">Send me a message:</h2>
 
-            <a href="home" className="contact__link">
-                <p className="contact__link-type">Email <FaRegEnvelope className='contact__link-type-icon'/></p>
-            </a>
-            <p className="contact__link-handle">gary@garyk.dev</p>
+                <form id='message' className='contact__message-form'>
+                    <label>
+                        Subject
+                        <input 
+                            name='subjectInput'
+                            type='text'
+                            value={subjectInput}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <label>
+                        Message
+                        <textarea 
+                            name='messageInput'
+                            type='text'
+                            value={messageInput}
+                            onChange={handleChange}
+                            required
+                        />
+                    </label>
+                    <button type='submit'>Send</button>
+                </form>
 
-            <a href="https://www.linkedin.com/in/garykassee/" target="_blank" rel="noreferrer" className="contact__link">
-                <p className="contact__link-type">LinkedIn <FaLinkedinIn className='contact__link-type-icon'/></p>
-            </a>
-            <p className="contact__link-handle">/in/garykassee</p>
-
-            <a href="https://twitter.com/gareWare" target="_blank" rel="noreferrer" className="contact__link">
-                <p className="contact__link-type">Twitter <FaTwitter className='contact__link-type-icon'/></p>
-            </a>
-            <p className="contact__link-handle">gareWare</p>
-            
+            </div>
         </div>
     )
 }
-
-//             <h2 className="contact__form-header">Or send me a message:</h2>
