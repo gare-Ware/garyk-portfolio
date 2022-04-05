df7c701d155227c8feea56e7f01f476997dd70f0
gareWare <92345400+gare-Ware@users.noreply.github.com>
2022-04-04 23:45:16 -0400

    Beta build

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,6 @@ import { useState } from "react"
 export default function Navbar() {
     
     const [menuOpen, setMenuOpen] = useState(false)
-    
     const toggleMenu = () => setMenuOpen(!menuOpen)
     const closeMenu = () => setMenuOpen(false)
 
@@ -16,21 +15,21 @@ export default function Navbar() {
                 </div>
                 <nav className="navbar__menu-large-screen">
                     <ul className="navbar__menu-items" onClick={closeMenu}>
-                        <li><a href="#top" className="active">Home</a></li>
+                        <li><a href="#top">Home</a></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#recent-projects">Recent projects</a></li>
                     </ul>
-                    <a href="/#contact" className="navbar__contact navbar__contact-large-screen" onClick={closeMenu}>Contact</a>
+                    <a href="#contact" className="navbar__contact navbar__contact-large-screen" onClick={closeMenu}>Contact</a>
                 </nav>
-                <a href="/#contact" className="navbar__contact" onClick={closeMenu}>Contact</a>
+                <a href="#contact" className="navbar__contact" onClick={closeMenu}>Contact</a>
             </header>
             <nav className={menuOpen ? "navbar__menu active" : "navbar__menu"}>
                 <h1 className="navbar__menu-header">Jump to</h1>
                 <ul className="navbar__menu-items" onClick={closeMenu}>
-                    <li><a href="/#top">Home</a></li>
-                    <li><a href="/#about">About</a></li>
-                    <li><a href="/#recent-projects">Recent projects</a></li>
-                    <li><a href="/#contact">Contact</a></li>
+                    <li><a href="#top">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#recent-projects">Recent projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
             </nav>
         </>
